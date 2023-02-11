2554e6db4ce94eb0ee5ea6c6abda34d2434635f2
divinejoshua <divineuche399@gmail.com>
2023-02-11 22:54:00 +0000

    Added openai api from docs

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import { Configuration, OpenAIApi } from "openai";
 import { useState } from 'react';
 
 function App() {
+
+  // completion function 
+  const completion = async () => {
+    
+    // Generate image 
+    const response = await openai.createImageVariation(
+      fs.createReadStream("corgi_and_cat_paw.png"),
+      1,
+      "1024x1024"
+    );
+    image_url = response.data.data[0].url;
+
+  }
+
+
+
   return (
-    <div className="App">
+    <div className="App mx-auto mt-20 max-w-screen-sm p-7">
+
+      
       
     </div>
   );
