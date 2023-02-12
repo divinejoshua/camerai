c5523d6239338c3be16eadcbc401a3f163b8359c
divinejoshua <divineuche399@gmail.com>
2023-02-12 14:10:30 +0000

    Added model

--- camerai/models.py
+++ camerai/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+def upload_location(instance, filename):
+	file_path = 'images/{filename}'.format(
+			filename=filename)
+	return file_path
+
+class OrginalImage(models.Model):
+    image           = models.ImageField(upload_to=upload_location, null=False, blank=False)
+    date_published 	= models.DateTimeField(auto_now_add=True, verbose_name="date published")
+
+class GenertedImages(models.Model):
+    generatedImage  = models.ForeignKey(OrginalImage, on_delete=models.CASCADE)
+    orginalImage    = models.ImageField(upload_to=upload_location, null=False, blank=False)
+    date_published 	= models.DateTimeField(auto_now_add=True, verbose_name="date published")
