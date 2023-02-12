eb56fff1a259b7185f4f1eef4325d3ede94cd856
divinejoshua <divineuche399@gmail.com>
2023-02-12 13:16:13 +0000

    Made canges

--- camerai/views.py
+++ camerai/views.py
@@ -7,5 +7,4 @@ class HomeView(TemplateView):
 
     # Get request 
     def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name)
